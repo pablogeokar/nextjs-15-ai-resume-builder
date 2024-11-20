239c83cff5e1e76f369f7ed1899b3fbe2f17bf14
Pablo George <pablogeokar@hotmail.com>
2024-11-19 21:16:08 -0300

    feat: criação das validações com zod e início da criação dos forms do editor

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Your resumes",
+};
+
 export default function Page() {
-  return <main>Here will be your resumes</main>;
+  return (
+    <main className="max-w-7xl mx-auto w-full px-3 py-6 space-y-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href="/editor">
+          <PlusSquare className="size-5" />
+          New resume
+        </Link>
+      </Button>
+    </main>
+  );
 }
